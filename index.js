8ad67b51af1a8bc0039f119246504c1b04c16e44
saifulaija <saifulaija2023@gmail.com>
2023-05-19 00:59:45 +0600

    delete data implemented

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express =require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -90,6 +90,15 @@ app.post('/setToys', async(req, res)=>{
       const result = await toysCollection.insertOne(body);
       res.send(result);
 })
+
+
+app.delete('/setToys/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await toysCollection.deleteOne(query);
+      res.send(result);
+})
+
 
 
 
