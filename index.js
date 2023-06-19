489eca4350f089a8c76e07c67f18eec100a0115e
saifulaija <saifulaija2023@gmail.com>
2023-06-19 21:22:31 +0600

    sort again implemented

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ async function run() {
     app.get("/update/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await toysCollection.findOne(query);
+      const result = await toysCollection.findOne(query)
       res.send(result);
     });
 
